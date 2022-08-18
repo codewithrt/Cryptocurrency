5fa5c1f4b0513cfda78d1d60000bf5b0cf8651e1
codewithrt <97937663+codewithrt@users.noreply.github.com>
2022-08-18 18:10:14 +0530

    firstcommit

--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -8,9 +8,9 @@ module.exports = {
       url: "HTTP://127.0.0.1:7545",
     },
     georli: {
-      url: "https://goerli.infura.io/v3/7f2148d884c74dfb811443b3f1b50291",
+      url: "https://goerli.infura.io/v3",
       accounts: [
-        "0x816f065dc0f01ae8c67c916cc310b2524137432cd018eb20a1f01c8c192d3017","0x52b29e8b2117cc4af132cf7b58a771000120a4175c74f412148bda3d20159552",
+        "0x","0x",
       ],
     },
   },
